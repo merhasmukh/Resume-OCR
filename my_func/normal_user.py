b757450fd5523baf104efc0eab7a6f073e22c9c6
mhgn <mhgn0001@gmail.com>
2023-03-01 15:01:26 +0530

    ./my_func/normal_user.py added & requirements.txt  added

--- my_func/normal_user.py
+++ my_func/normal_user.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 
 def pdf_file():
-    pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+    try:
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+    except Exception as e:
+        st.write(str(e))
