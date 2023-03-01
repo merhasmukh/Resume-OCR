7dfc738a8dba7122c08f4aa5d660beff739779e3
mhgn <mhgn0001@gmail.com>
2023-03-01 20:13:18 +0530

    /my_func/show_pdf.py added

--- my_func/normal_user.py
+++ my_func/normal_user.py
@@ -1,8 +1,19 @@
 import streamlit as st
-
+from my_func import show_pdf
+import os
 
 def pdf_file():
     try:
         pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            if os.path.exists("./resume_data") == False:
+                os.mkdir("./resume_data")
+            pdf_path="./resume_data"
+            
+            with open(pdf_path+"/"+pdf_file.name,'wb') as f:
+                f.write(pdf_file.getvalue())
+            pdf_file_path=pdf_path+"/"+pdf_file.name
+            show_pdf.display_pdf(pdf_file_path)
+     
     except Exception as e:
         st.write(str(e))
---
+++ my_func/show_pdf.py
@@ -0,0 +1,13 @@
+
+import base64
+import streamlit as st
+
+def display_pdf(file_path):
+    try:
+        with open(file_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+    except Exception as e:
+        st.write(str(e))
