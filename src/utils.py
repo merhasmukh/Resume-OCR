1b190f84a001dc23e85d559bfe2f5f358483ee7b
mer hasmukh <55354471+mhgn@users.noreply.github.com>
2023-03-17 17:48:25 +0530

    update utils

--- src/utils.py
+++ src/utils.py
@@ -23,20 +23,45 @@ def getTime():
     
 def create_txt_file(filename,text):
     try:
-        print("text",text)
-        if os.path.exists(filename+".txt"):
-            with open(filename+".txt","a") as fb:
+        # text_file=filename+".txt"
+        text_file=filename  
+        print("text_file_path_2",text_file)
+
+        if os.path.exists(text_file):
+            with open(text_file,"a") as fb:
                 for line in text:
                     fb.write(line+"\n")
                 fb.close()
         else:
-            with open(filename+".txt","a") as fb:
+            with open(text_file,"a") as fb:
                 for line in text:
                     fb.write(line+"\n")
                 fb.close()
 
     except Exception as e:
-        st.error("error in  create_txt_file"+str(e))
+        st.error(str(e))
+
+        st.error("error in  create_txt_file")
+
+def delete_file(filename):
+    try:
+        os.remove(filename)
+        
+    except Exception as e:
+        st.error(str(e))
+        st.error("error in  delete file")
+
+def read_txt_file(filename):
+    try:
+        with open(filename,"r") as fr:
+           text_list=fr.readlines()
+           fr.close()
+
+        return text_list
+        
+    except Exception as e:
+        st.error(str(e))
+        st.error("error in  delete file")
 
 
 # print(getDate())
